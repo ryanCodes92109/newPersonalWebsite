6f84a116652c843eae3c40521b5d211eb2edbbbc
Ryan <sullivan050ryan@gmail.com>
2023-03-25 20:38:09 -0700

    more styling

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -6,21 +6,28 @@ const AboutMe = () => {
     <>
  
     <div className='aboutMeContainer'>
-    <div className='aboutMePhotoreel'>
-      Insert Photo reel
-    </div>
+
     <br/>
         <div className='aboutMeInfo'>
-            <p> i'll put some text and pictures about me </p> 
+
+          <p> 
+            Hi! I'm Ryan, and i am a Fullstack Engineer. I've made a huge change in my life and decided to learn some new skills that would allow me to expand more personally and professionally. I'm an ex-Technical Recruiter with companies like Google, Zillow and Rivian, but finally had the chance to pursue a different career path after 10 years. 
+          </p>
+         
+          <p>
+            I completed the Flatiron School Fullstack Engineering program in 2023 and really enjoyed building my <strong>projects</strong> while getting a strong grasp on Javascript, React and Ruby/Rails.     I really enjoyed working closely with the Frontend of my app specifically and really hope I get the chance to utilize my creativity and enhance my UI skills!
+          </p>
+
+          <p>
+            Feel free to <strong>contact</strong> me if I may be a good fit for any open positions you know of! In the meantime, here are a bunch of pictures of my amazing furry friends.
+          </p> 
+ 
         </div>
 
-        <div className='aboutMeInfo'>
-            <p> i'll put some text and pictures about me </p> 
-        </div>
+        <div className='aboutMePhotoreel'>
+      Insert Photo reel
+    </div>
 
-        <div className='aboutMeInfo'>
-            <p> i'll put some text and pictures about me </p> 
-        </div>
     </div>
     </>
   )
