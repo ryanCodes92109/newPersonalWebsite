8b97fa906c592d2d80e1e1745c007cd0b1dd2be2
Ryan Sullivan <ryansullivan@Ryans-MBP.attlocal.net>
2023-03-27 21:15:33 -0700

    after rebase

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -8,10 +8,12 @@ const AboutMe = () => {
       <br/>
           <div className='aboutMeInfo'>
             <p> 
-              Hi! I'm Ryan, and i am a Fullstack Engineer. I've made a huge change in my life and decided to learn some new skills that would allow me to expand more personally and professionally. I'm an ex-Technical Recruiter with companies like Google, Zillow and Rivian, but finally had the chance to pursue a different career path after 10 years. 
+
+              Hi! I'm Ryan, and I am a Fullstack Engineer. I've made a huge change in my life and decided to learn some new skills that would allow me to expand more personally and professionally. I'm an ex-Technical Recruiter with companies like Google, Zillow and Rivian, but finally had the chance to pursue a different career path after 10 years. 
+
             </p>
             <p>
-              I completed the Flatiron School Fullstack Engineering program in 2023 and really enjoyed building my <strong>projects</strong> while getting a strong grasp on Javascript, React and Ruby/Rails.     I really enjoyed working closely with the Frontend of my app specifically and really hope I get the chance to utilize my creativity and enhance my UI skills!
+              I completed the Flatiron School Fullstack Engineering program in 2023 and really enjoyed building my <strong>projects</strong> while getting a strong grasp on Javascript, React and Ruby/Rails. I really enjoyed working closely with the Frontend of my app specifically and really hope I get the chance to utilize my creativity and enhance my UI skills!
             </p>
             <p>
               Feel free to <strong>contact</strong> me if I may be a good fit for any open positions you know of! In the meantime, here are a bunch of pictures of my dogs.
