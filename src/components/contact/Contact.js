5ef05cd60e87572571a045c9ec18956531108bb8
Ryan Sullivan <ryansullivan@Ryans-MBP.attlocal.net>
2023-03-29 15:38:15 -0700

    style contact page and form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import './Contact.css'
 import { TextField } from '@mui/material'
+import {TextareaAutosize} from '@mui/material'
 import { send } from 'emailjs-com'
 
 const Contact = () => {
@@ -31,68 +32,87 @@ const Contact = () => {
     }
 
   return (
+    <>
+        <div className = 'contactContainer'>
 
-    <div className = 'contactContainer'>
-        <form 
-            onSubmit={handleSubmit}
-            className='contactSubmitForm'>
-            <TextField
-                className='submitInput'
+            <form 
+                onSubmit={handleSubmit}
+                className='contactSubmitForm'>
+
+                <TextField
+                    required
+                    variant="filled"
+                    className='submitInput'
+                    sx={{
+                        width:250
+                        }}
+                    label="Name"
+                    onChange={handleChange}
+                    name='from_name'
+                    value={toSend.from_name}
+                ></TextField>
+                <br/>
+
+                <TextField
+                required
+                variant="filled"
                 sx={{
                     width:250
-                    }}
-                label="Name"
-                onChange={handleChange}
-                name='from_name'
-                value={toSend.from_name}
-            ></TextField>
-
-            <TextField
-              sx={{
-                width:250
-            }}
-                className='submitInput'
-                label="Email"
-                onChange={handleChange}
-                name='reply_to'
-                value={toSend.reply_to}
-            ></TextField>
-
-            <TextField
-                className='submitInput'
-                label='Message'
-                sx={{
-                    width:275,
-                    // "& .MuiInputBase-root": {
-                    //     height: 160
-                    // }
                 }}
-                onChange={handleChange}
-                name='message'
-                value={toSend.message}
-            >
-            </TextField>
+                    className='submitInput'
+                    label="Email"
+                    onChange={handleChange}
+                    name='reply_to'
+                    value={toSend.reply_to}
+                ></TextField>
+                <br/>
 
+                {/* <TextareaAutosize */}
+                <TextField
+                    required
+                    
+                    variant="filled"
+                    className='submitInput'
+                    label='Write your message here'
+                    size='large'
+                    multiline
+                    minRows={5}
+                    sx={{
+                        width:250
+                    }}
+                    
+                    onChange={handleChange}
+                    name='message'
+                    value={toSend.message}>
+                </TextField>
+                    
+                {/* </TextareaAutosize> */}
+                <br/>
 
-            <button 
-                type='submit'
-                className='emailButton'
-            >Send</button>
+                <button 
+                    type='submit'
+                    className='emailButton'
+                >Send</button>
 
-        </form>
+            </form>
+
+            <div className='socialLinkContainer'>
+                
+                <a href='https://www.linkedin.com/in/ryansul/'>
+                    <img
+                        className='linkedinProfileLink'
+                        src={'./linkedinlog.jpg'}/>
+                </a>
+
+            </div>
         
-        <div
-            className='socialLinkContainer'
-        >
-        <br/>
-            <a href='https://www.linkedin.com/in/ryansul/'>
-                <img
-                    className='linkedinProfileLink'
-                    src={'./linkedinlog.jpg'}
-                />
-            </a>
         </div>
-    </div>
+
+
+            
+        
+           
+    </>
   )
 }
 
