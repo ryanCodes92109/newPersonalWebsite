ca134af1451edc9df7ba0c9b9d562b7ade069b36
Ryan Sullivan <ryansullivan@Ryans-MBP.attlocal.net>
2023-04-08 09:43:44 -0700

    navbar update

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,30 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import { FiAlignJustify } from "react-icons/fi";
+
 
 const Navbar = () => {
+
+  const [sidebar, setSideBar] = useState(false)
+
+const showSidebar = () => setSideBar(!sidebar)
   
   return (
     <div className='header'>
       
       <span className='titleText'>Ryan Sullivan - <span className='fullstackTitle'>Fullstack Engineer</span></span>
-        <div className='navbar'>
-          <Link to='/'>
-            <li className='navbutton'>About Me</li>
-          </Link>
+        <div className= {sidebar ? 'navbarActive' :'navbarHidden'}>
 
-          {/* <Link to='/projects'>
-            <li className='navbutton'>Projects</li>
-          </Link> */}
+       
+        <Link to='/'>
+          <li className='navbutton'>About Me</li>
+        </Link>
 
-          <Link to='/resume'>
-            <li className='navbutton'>Resume</li>
-          </Link>
+        <Link to='/resume'>
+          <li className='navbutton'>Resume</li>
+        </Link>
 
-          <Link target='_blank' to='https://www.linkedin.com/in/ryansul/'>
-            <li className='navbutton'>LinkedIn</li>
-          </Link>
+         
+        <Link target='_blank' to='https://www.linkedin.com/in/ryansul/'>
+          <li className='navbutton'>LinkedIn</li>
+        </Link> 
+          
         </div>
+
+        <Link to='#' className='menuIcon'>
+         <FiAlignJustify onClick={showSidebar} size={25}/>
+       </Link> 
     </div>
   )
 }
---
+++ src/components/navbar/SidebarData.js
@@ -0,0 +1,36 @@
+import React from 'react'
+// import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+import * as IoIcons from 'react-icons/io'
+
+
+export const SidebarData = [
+   {
+       title: ' About Me',
+       path:'/',
+       icon: <AiIcons.AiFillHome/>,
+       cName:'navText'
+   },
+
+
+   {
+       title: ' Projects',
+       path:'/projects',
+       icon: <IoIcons.IoIosPaper/>,
+       cName:'navText'
+   },
+   {
+       title: ' Resume',
+       path:'/resume',
+       icon: <AiIcons.AiFillHome/>,
+       cName:'navText'
+   },
+   {
+       title: 'Linkedin',
+       path:'https://www.linkedin.com/in/ryansul/',
+       icon: <AiIcons.AiFillHome/>,
+       cName:'navText'
+   }
+]
+
+
